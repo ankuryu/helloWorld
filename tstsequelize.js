2c09e12b2475f2d4d2d089b70e198e1a504d6192
sunil <sunil@vpg>
2018-04-25 18:44:06 +0530

    More with Database

--- tstsequelize.js
+++ tstsequelize.js
@@ -1,6 +1,7 @@
 /* jshint  esversion:6 */
 const Sequelize = require('sequelize');
 const sequel = new Sequelize('sqlite:./db/quot18.db3');
+const Op = Sequelize.Op
 sequel
   .authenticate()
   .then(() => {
@@ -75,25 +76,40 @@ const Quotc= sequel.define('quotc',
 	},{
 		freezeTableName: true,
 	timestamps: false});
-sequel.sync().then(function(){
-	Quotop.create({
+sequel.sync().then(() =>{console.log("creating RJ"); 
+	Quotop.create(
+		{
 		qno: '00008',
 		qdt: '2018-04-09',
 		pname: 'R Jayeshkumar & Co',
-		paddr: '84, Nagdevti Street, Mumbai 400 003',
-	});
-		Quotop.findAll().then(quotations=> {
-		console.log(quotations.length);
-                sequel.query('Select * from quotop;').then(quote=>{console.log(quote);
-			sequel.close().then(()=>{console.log("connection closed");});
-		});
-	});
-}) ;
+		paddr: '84, Nagdevti Street, Mumbai 400 003'
+		})
+}).then( ()=> {
+	    console.log("finding all");
+	Quotop.findAll().then((quotations)=> 
+		{
+			console.log(quotations.length);
+			console.log("total Quotations");
+		})
 
-
-
-/*sequel.close().then(function() {
-	console.log("connection closed");
-}); */
-
-
+	}).then( ()=> {
+		console.log("Destroyer in action");
+		Quotop.destroy( {where:{id : {[Op.gt]: 7}}})
+	}).then( ()=> {
+		console.log("bulk Creator in action")
+		quotes = [{qno:"00012"},{qno:"00013"}]
+		Quotop.bulkCreate(quotes);
+	
+	}).then( ()=> {
+		Quotop.update( {pname:"Kapoor & Co"},{where:{id:{[Op.eq]: 10}}}).catch((error)=>{console.log(error)})
+	
+	}).then(  ()=> {
+		Quotop.findAll( {where: {pname: {[Op.eq]:null}}}).then((results)=>{
+			console.log(results[0].get({plain:true}));	
+		})
+	}).then(  function() {
+		process.nextTick( ()=>{
+		sequel.close().catch( (error)=>{console.log(error)});
+		console.log("connection closed");	
+		})
+	}).catch((error)=> {console.log(error )})
